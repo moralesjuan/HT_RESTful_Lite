7996eb28b33bf43cd5bb397ca44f603ee4610510
Juan Antonio Morales Portillo <jmorales@sar.gob.hn>
2021-12-06 03:06:23 -0600

    Creation CupsController

--- HT_RESTful_Lite/Controllers/CupsController.cs
+++ HT_RESTful_Lite/Controllers/CupsController.cs
@@ -32,12 +32,44 @@ namespace HT_RESTful_Lite.Controllers
             return Ok(cup);
         }
 
+        //[HttpGet("Details/{cupId:int}")]
+        //public async Task<IActionResult> GetCupDetails(int cupId)
+        //{
+        //    var list = await _context.CupDetails.Where(c => c.CupId == cupId)
+        //                                        .Include(c => c.Cup)
+        //                                        .ToListAsync();
+        //    return Ok(list);
+        //}
+
         [HttpGet("Details/{cupId:int}")]
-        public async Task<IActionResult> GetCupDetails(int cupId)
+        public IActionResult GetCupDetails(int cupId)
         {
-            var list = await _context.CupDetails.Where(c => c.CupId == cupId)
-                                                .Include(c => c.Cup)
-                                                .ToListAsync();
+            string query = $"EXECUTE dbo.CupFullDetails {cupId}";
+
+            List<CupDetailsResult> list = new List<CupDetailsResult>();
+
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = query;
+                _context.Database.OpenConnection();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var cupDetailsResult = new CupDetailsResult();
+                        cupDetailsResult.CupId = (int)reader.GetValue(0);
+                        cupDetailsResult.CupName = (string)reader.GetValue(1);
+                        cupDetailsResult.CupType = (string)reader.GetValue(2);
+                        cupDetailsResult.CupSize = (int)reader.GetValue(3);
+                        cupDetailsResult.Round = (int)reader.GetValue(4);
+                        cupDetailsResult.Local = (string)reader.GetValue(5);
+                        cupDetailsResult.RankingLocal = (int)reader.GetInt64(6);
+                        cupDetailsResult.Visitor = (string)reader.GetValue(7);
+                        cupDetailsResult.RankingVisitor =(int)reader.GetInt64(8);
+                        list.Add(cupDetailsResult);
+                    }
+                }
+            }
             return Ok(list);
         }
 
@@ -45,7 +77,6 @@ namespace HT_RESTful_Lite.Controllers
         public IActionResult CreateCup(Cups cups)
         {
             string query = "";
-            var result = 0;
 
             if (cups.Type == "HighLow")
             {
@@ -59,14 +90,7 @@ namespace HT_RESTful_Lite.Controllers
             {
                 query = $"EXECUTE dbo.CreateRandomCup '{cups.Name}', {cups.Size}";
             }
-
-            //var newCup = await _context.Database.ExecuteSqlRawAsync(query).
-            //            .FromSqlRaw(query)
-            //            .FirstOrDefaultAsync();
-
-            ////_context.Database.ExecuteSqlInterpolated() .ExecuteSqlCommand("CreateStudents @p0, @p1", parameters: new[] { "Bill", "Gates" });
-            //return Ok(newCup);
-
+            Cups CupResult = new Cups();
 
             using (var command = _context.Database.GetDbConnection().CreateCommand())
             {
@@ -76,11 +100,28 @@ namespace HT_RESTful_Lite.Controllers
                 {
                     while (reader.Read())
                     {
-                        result = (int)reader.GetValue(0);
+                        CupResult.CupId = (int)reader.GetValue(0);
+                        CupResult.Name = (string)reader.GetValue(1);
+                        CupResult.Type = (string)reader.GetValue(2);
+                        CupResult.Size = (int)reader.GetValue(3);
                     }
                 }
             }
-            return Ok(result);
+            return Ok(CupResult);
+        }
+
+        private class CupDetailsResult
+        {
+            public int CupId { get; set; }  
+            public string CupName { get; set; }
+            public string CupType { get; set; }
+            public int CupSize { get; set; }
+            public int Round { get; set; }
+            public string Local { get; set; }
+            public int RankingLocal { get; set; }
+            public string Visitor { get; set; }
+            public int RankingVisitor { get; set; }
+
         }
     }
 }
